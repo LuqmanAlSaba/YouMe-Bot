a0fe5b9e035202214b1a27ec1325b9f1323a1696
sethBAR <seth_baunach@jabil.com>
2021-11-29 17:04:42 -0500

    initial work

---
+++ command_stream.py
@@ -0,0 +1,51 @@
+
+class YouMeCommandStream:
+
+    # function for appending text to command queue
+
+        # (do we need a queue? should we wait for command to finish before executing another?
+        #  i'd assume so, since commands involve output over time, multiple running at once is bad
+        #  but we don't want to stop parsing commands while running a command
+
+    # essentially wraps an IOStream, but instead of writing to a file,
+    # writes to a "command text" buffer and enqueues valid commands
+    # commands are executed in-order as soon as they are added to the command queue
+    # a command is specified by an exact string match (not case sensitive)
+    # when a command is matched, the corresponding function is run
+    def __init__(self, command_dict, buffering=False):
+        # queue of function calls
+        self.function_call_queue = []
+        # buffer for storing voice input, word-by-word
+        self.word_buffer = []
+        # dictionary of valid commands
+        self.command_dict = command_dict
+
+
+    def write(self, text):
+        for word in text.split():
+            self.word_buffer.append(word)
+
+    def flush(self):
+        # greedily try building commands
+
+
+    # way to specify valid commands
+
+    # way to tie commands to function calls with arguments
+
+
+
+
+
+if __name__ == '__main__':
+    def back():
+        print("back command call")
+
+    def buy(item_name):
+        print("buy command call with arg: " + item_name)
+    command_dict_example = {
+        "back", [back],
+        "buy item one", [buy, "item one"],
+        "buy item two", [buy, "item two"]
+    }
+    ym_cs = YouMeCommandStream(command_dict_example)
