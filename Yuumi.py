e242d4f8245685ecec165d8959269ee927d7f7c2
Luke Al-Saba <luke@al-saba.com>
2021-11-30 17:02:48 -0500

    Added warding, leveling up abilities, and in-game messages. Made improvements to buying function.

--- Yuumi.py
+++ Yuumi.py
@@ -2,46 +2,80 @@ import time
 import keyboard
 import pyautogui
 import pydirectinput
+import speech_recognition
 import speech_recognition as sr
 import pyaudio
 import requests
 from urllib3.exceptions import InsecureRequestWarning
+import threading
+import random
+
+# get live game data: https://127.0.0.1:2999/liveclientdata/allgamedata
+# https://developer.riotgames.com/docs/lol#game-client-api_live-client-data-api
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 screen_center = int(pydirectinput.size()[0] / 2.0), int(pydirectinput.size()[1] / 2.0)
 
+purchased_items = []
+item_path = [('Spellthief\'s Edge', 400), ('Faerie Charm', 250), ('Amplifying Tome', 435), ('Bandleglass Mirror', 265), ('Ruby Crystal', 400), ('Kindlegem', 400), ('Moonstone Renewer', 750),
+             ('Amplifying Tome', 435), ('Oblivion Orb', 400), ('Faerie Charm', 250), ('Amplifying Tome', 435), ('Bandleglass Mirror', 265), ('Chemtech Putrifier', 550),
+             ('Amplifying Tome', 435), ('Faerie Charm', 250), ('Forbidden Idol', 550), ('Amplifying Tome', 435), ('Ardent Censer', 630),
+             ('Amplifying Tome', 435), ('Faerie Charm', 250), ('Forbidden Idol', 550), ('Amplifying Tome', 435), ('Staff of Flowing Water', 630),
+             ('Faerie Charm', 250), ('Forbidden Idol', 550), ('Null-Magic Mantle', 450), ('Negatron Cloak', 450), ('Mikael\'s Blessing', 600),
+             ('Dark Seal', 350), ('Mejai\'s Soulstealer', 1250)]
 
-# def q():
-#
+roles = {
+        'top': 'f1',
+        'jungle': 'f2',
+        'mid': 'f3',
+        'adc': 'f4',
+        'support': 'f5'
+    }
 
 
-# Yuumi dashes to a target ally
-def w(player):
-    pydirectinput.keyDown(player)
+def q():
+    keyboard.press_and_release('q')
+
+
+# Yuumi dashes to a target ally when given an allies position
+# example: w jungle
+def w(position):
+    pydirectinput.keyDown(roles[position])
     pydirectinput.moveTo(screen_center[0], screen_center[1] - 50, duration=0)
     keyboard.press_and_release('w')
 
 
-# def e():
-#
+def e():
+    keyboard.press_and_release('e')
+
 
 
 # def r():
 #
 
+
 # Yuumi performs the back action, detaching from allies if necessary
 def back():
     data = get_data()
-    attached_status = data['activePlayer']['abilities']['W']['id']
-    if attached_status == 'YuumiWEndWrapper':
-        pydirectinput.keyDown('w')
-    pydirectinput.keyDown('b')
+    try:
+        attached_status = data['activePlayer']['abilities']['W']['id']
+        if attached_status == 'YuumiWEndWrapper':
+            pydirectinput.press('w')
+    except TypeError:
+        print("Not in game")
+    pydirectinput.press('b')
 
 
-# Yuumi follows a target ally
-def follow(player):
-    pydirectinput.keyDown(player)
+def ward():
+    pydirectinput.moveTo(screen_center[0] - 10, screen_center[1] + 25)
+    pydirectinput.keyDown('4')
+
+
+# Yuumi follows a target ally when given an allies position
+# example: follow jungle
+def follow(position):
+    pydirectinput.keyDown(roles[position])
     pydirectinput.keyDown('x')
     pydirectinput.rightClick(screen_center[0] - 10, screen_center[1] + 25, duration=10000)
 
@@ -79,57 +113,37 @@ def get_data():
         print("Game has ended.")
 
 
-if __name__ == '__main__':
-    # get live game data: https://127.0.0.1:2999/liveclientdata/allgamedata
-    # https://developer.riotgames.com/docs/lol#game-client-api_live-client-data-api
-
-    purchased_items = []
-    item_path = [('Spellthief\'s Edge', 400), ('Faerie Charm', 250), ('Amplifying Tome', 435),
-                 ('Bandleglass Mirror', 265), ('Ruby Crystal', 400), ('Kindlegem', 400)]
-
-    # Below is code for testing the functions while we don't have VR working
-    # We can add more functions to the if/else branch as we code them
-    print("+---------------------------+")
-    print("+       Select Option       +")
-    print("+       W - W Ability       +")
-    print("+       B - Recall          +")
-    print("+       O - See Options     +")
-    print("+       STOP - Stop Testing +")
-    print("+---------------------------+")
+def level_up_abilities():
+    kill_messages = ['nice!', 'ez', ':)', 'gj', 'well played', 'FF', 'SO EZ']
+    ability_order = ["q", "e", "e", "q", "e", "r", "e", "q", "e", "r", "q", "q", "w", "w", "r", "w", "w"]
+    yuumi_level = 0
+    team_kills = 0
     while True:
-        choice = input()
-        if choice == 'W':
-            print("Executing W in 5 seconds, please open LoL...")
-            time.sleep(5)
-            w('f4')
-        elif choice == 'B':
-            print("Executing B in 5 seconds, please open LoL...")
-            time.sleep(5)
-            back()
-        elif choice == 'O':
-            print("+---------------------------+")
-            print("+       Select Option       +")
-            print("+       W - W Ability       +")
-            print("+       B - Recall          +")
-            print("+       O - See Options     +")
-            print("+---------------------------+")
-        elif choice == 'STOP':
-            break
-        else:
-            print("Please select a valid option...")
+        data = get_data()
 
-    # follow('f3')
-    # w('f4')
-    # e()
+        current_level = int(data['activePlayer']['level'])
+        current_team_kills = 0
 
-    # Example voice recognition code that listens
-    # for 5 seconds and prints the result
-    r = sr.Recognizer()
+        # if yuumi has leveled up
+        if current_level != yuumi_level:
+            # level up next ability in queue
+            next_ability = ability_order.pop(0)
+            pydirectinput.keyDown('ctrl')
+            pydirectinput.keyDown(next_ability)
+            pydirectinput.keyUp('ctrl')
+            pydirectinput.keyUp(next_ability)
+            yuumi_level += 1
 
-    with sr.Microphone() as source:
-        # read the audio data from the default microphone
-        audio_data = r.record(source, duration=5)
-        print("Recognizing...")
-        # convert speech to text
-        text = r.recognize_google(audio_data)
-        print(text)
+        # get updated team kills
+        for player in data['allPlayers']:
+            current_team_kills += int(player['scores']['kills'])
+
+        # if team gets a kill
+        if team_kills != current_team_kills:
+            # emote
+            pydirectinput.press('t')
+            # Type message in chat
+            pydirectinput.press('enter')
+            pyautogui.write('/all ' + random.choice(kill_messages))
+            pydirectinput.press('enter')
+        time.sleep(2)
